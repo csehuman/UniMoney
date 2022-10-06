1ae61c907ad8059e135f91734abe7bbe207e7173
csehuman <paullee20204@snu.ac.kr>
2022-10-06 12:47:21 +0900

    Initial Commit

---
+++ UniMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniMoney
+//
+//  Created by Paul Lee on 2022/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
